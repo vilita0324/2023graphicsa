b09d0959b2b6abf12ca8fc0391ed6dbb0fbe7577
vilita0324 <dadalin503@gmail.com>
2023-04-19 12:10:25 +0800

    add week10

---
+++ week10_glm/main.cpp
@@ -0,0 +1,25 @@
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel = NULL;///有一個模型的指標，先是空的
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(pmodel == NULL){
+        pmodel = glmReadOBJ("AL.obj");
+        glmUnitize(pmodel);
+        glmFacetNormals(pmodel);
+        glmVertexNormals(pmodel,90);
+    }
+    glmDraw(pmodel,GLM_MATERIAL);
+    ///glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+int main(int argc,char* argv[])
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week10");
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
